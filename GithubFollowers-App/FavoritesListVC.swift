299bc745f16d6b8d2618511a338bfa5371ae876d
Furkan Bingöl <furkanbingol176@gmail.com>
2023-08-22 20:36:14 +0300

    UITabBarController,UINavigationController Setup & No Storyboard Approach

---
+++ GithubFollowers-App/FavoritesListVC.swift
@@ -0,0 +1,27 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers-App
+//
+//  Created by Furkan Bingöl on 22.08.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    // MARK: - UI Elements
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    // MARK: - Functions
+    
+}
---
+++ GithubFollowers-App/SearchVC.swift
@@ -0,0 +1,28 @@
+//
+//  SearchVC.swift
+//  GithubFollowers-App
+//
+//  Created by Furkan Bingöl on 22.08.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    // MARK: - UI Elements
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    // MARK: - Functions
+    
+
+}
